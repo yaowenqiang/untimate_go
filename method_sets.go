e7b3ac51c36b49dc0b4665aa6eb0eeeb598bdefa
yaowenqiang <yaowenqiang111@163.com>
2020-11-27 01:53:25 +0800

    add polymossive course demo

---
+++ method_sets.go
@@ -0,0 +1,34 @@
+package main
+import "fmt"
+
+type notifier interface {
+    notify()
+}
+
+type user struct {
+    name string
+    email string
+}
+
+type duration int
+
+func (d *duration) notify() {
+    fmt.Printf("Sedning Notification in ", *d)
+}
+
+
+func (u *user) notify() {
+    fmt.Printf("Seding User Email TO %s<%s>\n",
+        u.name,
+        u.email)
+}
+func sendNotification(n notifier) {
+    n.notify()
+}
+
+func main() {
+    duration(42).notify()
+    u := user{"Bill", "bill@bill.com"}
+    sendNotification(u)
+}
+
